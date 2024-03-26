32ad36d9dd0f6436276f32087f284f59ef00ea51
Sam Law <samuel.law@system1group.com>
2024-03-26 12:59:42 +0000

    Fonts

--- src/_components/header/logo.tsx
+++ src/_components/header/logo.tsx
@@ -2,7 +2,7 @@ function Logo() {
   return (
     <div className="relative block text-white">
       <img src="/images/system1_White.svg" alt="System1 Logo" />
-      <p className="absolute bottom-1 left-12 pl-2 text-base whitespace-nowrap">
+      <p className="absolute bottom-1 left-12 pl-2 text-base font-bold whitespace-nowrap">
         SCA Manager
       </p>
     </div>
